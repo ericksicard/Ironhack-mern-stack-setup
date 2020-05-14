ad3f4e3cb520ba750a38bad2431609ddc4ec476c
esrfree <esrfree@gmail.com>
2020-05-14 18:17:41 -0400

    Connecting the server to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { MongoClient } from 'mongodb';
 
 import template from '../template'
 
@@ -34,4 +35,13 @@ let port = process.env.PORT || 3000
 app.listen(port, function onStart(err) {
     if (err) console.log(err) 
     console.info('Server started on port %s.', port)
+})
+
+//Connecting the server to MongoDB
+/*MongoClient is the driver that connects to the running MongoDB instance using its URL.
+It allows us to implement the database-related code in the backend.*/
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/Ironhack-mern-stack-setup';
+MongoClient.connect( url, (err, db) => {
+    console.log("Connected successfully to mongodb server")
+    db.close()
 })
