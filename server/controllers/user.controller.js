3d5f424e281e05c92d015f8739273085c5291343
ericksicard <esrfree@gmail.com>
2020-05-19 22:27:22 -0400

    Creating a new user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -10,7 +10,32 @@ messages when a Mongoose error occurs.*/
 import errorHandler from '../helpers/dbErrorHandler';
 import User from '../models/user.model';
 
-const create = (req, res, next) => { … }
+//Creating a new user
+/*This function creates a new user with the user JSON object that's received in the POST request from
+the frontend within req.body. The call to user.save attempts to save the new user in the database after
+Mongoose has performed a validation check on the data. Consequently, an error or success response is
+returned to the requesting client.
+The create function is defined as an asynchronous function with the async keyword, allowing us to use
+await with user.save(), which returns a Promise. Using the await keyword inside an async function causes
+this function to wait until the returned Promise resolves, before the next lines of code are executed.
+If the Promise rejects, an error is thrown and caught in the catch block.
+*/
+const create = async (req, res) => {
+    const user = new User( req.body )
+    try {
+        await user.save()
+        return res.status(200).json({
+            message: 'Successfully signed up!'
+        })
+    }
+    catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
+
+
 const list = (req, res) => { … }
 const userByID = (req, res, next, id) => { … }
 const read = (req, res) => { … }
