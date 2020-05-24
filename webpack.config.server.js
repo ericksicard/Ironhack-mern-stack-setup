1ef2efeda6b9432100dbb426e9521830a37e7921
ericksicard <esrfree@gmail.com>
2020-05-24 19:02:21 -0400

    Bundling image assets

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -27,6 +27,10 @@ const config = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: [ 'babel-loader' ]
+            },
+            {
+                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+                use: 'file-loader'
             }
         ]
     }
