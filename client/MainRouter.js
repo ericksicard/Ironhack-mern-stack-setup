7752bfea2f78294ae7db2bc0495bbfc96afec38b
ericksicard <esrfree@gmail.com>
2020-05-26 14:13:47 -0400

    The Signup component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -11,12 +11,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './core/Home';
 import Users from './user/Users';
+import Signup from './user/Signup';
 
 const MainRouter = () => (
     <div>
         <Switch>
             <Route exact path='/' component={Home} />
             <Route path="/users" component={Users}/>
+            <Route path="/signup" component={Signup}/>
         </Switch>
     </div>
 )
