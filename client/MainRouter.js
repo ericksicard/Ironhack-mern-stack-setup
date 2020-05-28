9b213811b8254a07b162ac26b9d52c040da1347b
ericksicard <esrfree@gmail.com>
2020-05-28 00:05:32 -0400

    The EditProfile component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -12,7 +12,10 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './core/Home';
 import Users from './user/Users';
 import Signup from './user/Signup';
+import EditProfile from './user/EditProfile';
 import Profile from './user/Profile';
+
+import PrivateRoute from './auth/PrivateRoute';
 
 const MainRouter = () => (
     <div>
@@ -20,6 +23,7 @@ const MainRouter = () => (
             <Route exact path='/' component={Home} />
             <Route path="/users" component={Users}/>
             <Route path="/signup" component={Signup}/>
+            <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
             <Route path="/user/:userId" component={Profile}/>
         </Switch>
     </div>
